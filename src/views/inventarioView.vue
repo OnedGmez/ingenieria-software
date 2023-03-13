85550571270d7d0899acd0257283d9103028beef
OnedGmez <oned.gomez@unah.hn>
2023-03-12 20:52:40 -0600

    Inicio de modulo de inventario

---
+++ src/views/inventarioView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MenuSpace
+  />
+  </div>
+</template>
+  
+<script>
+import MenuSpace from '@/components/menu.vue'
+export default {
+  name: 'Menu',
+  components: {
+    MenuSpace
+  }
+}
+</script>
+
+<style scoped>
+div{
+  height: 100%;
+  width: fit-content;
+}
+</style>
