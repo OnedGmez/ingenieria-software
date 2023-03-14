f2a39f607e696e85b1c3d0eb53f2a876382c032f
OnedGmez <oned.gomez@unah.hn>
2023-03-14 13:37:46 -0600

    Agregar los elementos del menu (json) a la store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import { defineStore } from 'pinia'
+import {ref} from 'vue'
+import menuOpciones from '@/jsons/elementosMenu.json'
 
 export const generalStore = defineStore('store', () => {
+  const menu = ref(menuOpciones.elementos);
 
-  return {}
+  return {
+    menu
+  }
 })
