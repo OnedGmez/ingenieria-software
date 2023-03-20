8e548934887267db2d95dbd81c65b14df368f2aa
OnedGmez <oned.gomez@unah.hn>
2023-03-20 17:23:14 -0600

    Versión maquetada de modulo de inventario(Incompleto)

--- src/views/inventarioView.vue
+++ src/views/inventarioView.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <MenuSpace
-  />
+  <div id="vista-inventario">
+    <div class="menu">
+      <MenuSpace />
+    </div>
+    <div class="cabecera">
+      <div class="titulo">
+        <span>INVENTARIO</span>
+      </div>
+      <div class="info-sucursal">
+        <div class="sucursal">
+          <span>Nombre Sucursal - Código Sucursal</span>
+        </div>
+        <div class="ubi-sucursal">
+          <span>Departamento - Colonia</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
   
@@ -16,8 +30,42 @@ export default {
 </script>
 
 <style scoped>
-div{
+#vista-inventario {
   height: 100%;
-  width: fit-content;
+  width: 100%;
+  display: inline-flex;
+  padding-right: 5px;
+}
+
+#vista-inventario .menu {
+  position: relative;
+}
+
+#vista-inventario .cabecera {
+  width: 100%;
+  height: fit-content;
+  position: relative;
+  display: flex;
+  margin: 10px 0 10px 8px;
+  text-align: center;
+}
+
+#vista-inventario .cabecera .titulo {
+  margin: auto;
+  font-weight: 900;
+  font-size: larger;
+  color: #3581B8;
+}
+
+#vista-inventario .cabecera .info-sucursal .sucursal {
+  /*Sujetas a cambio*/
+  font-weight: lighter;
+  font-size: smaller;
+}
+
+#vista-inventario .cabecera .info-sucursal .ubi-sucursal {
+  /*Sujetas a cambio*/
+  font-weight: lighter;
+  font-size:x-small;
 }
 </style>
