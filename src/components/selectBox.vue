808cfeb18e8eb8b93703d11da24e3a23bcfbec8c
OnedGmez <oned.gomez@unah.hn>
2023-03-26 09:33:45 -0600

    Versión primera del selectBox

---
+++ src/components/selectBox.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="select-group d-flex">
+        <label for="disabledSelect">{{ label }}</label>
+        <select @change="filtrar" id="disabledSelect" class="form-select">
+            <option>Disabled select</option>
+            <option>Disabled select2</option>
+            <option>Disabled select3</option>
+        </select>
+    </div>
+</template>
+
+<style scoped>
+
+.select-group{
+    flex-direction: column;
+}
+
+.select-group .form-select{
+    width: calc(165px + 1%);
+    border-radius: 25px 25px 25px 25px;
+    border: none;
+    border-bottom: #3581B8 solid 2px;
+    filter: none;
+    font-size: calc(.8em + 0.1vw) !important
+}
+
+.select-group{
+  font-size: small;
+}
+
+.select-group .form-select:focus{
+    box-shadow: none;
+}
+
+</style>
+
+<script setup>
+import { ref, defineEmits } from "vue";
+
+const propsTarjeta = defineProps([
+    'label',
+])
+
+const filtrar = (event) =>{
+    console.log(event.target.value)
+}
+</script>
