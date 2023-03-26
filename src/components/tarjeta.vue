8397e3719957828c54c9e0f4fbba92375c17cb89
OnedGmez <oned.gomez@unah.hn>
2023-03-26 01:09:58 -0600

    Actualización tarjeta (apta para todos los módulos)

--- src/components/tarjeta.vue
+++ src/components/tarjeta.vue
@@ -2,19 +2,29 @@
     <div class="col-lg-2">
         <div class="card">
             <div class="encabezado-img">
-                <img src="../assets/img/aceta.webp" class="card-img-top img-fluid" alt="...">
+                <img src="../assets/img/aceta.jpg" class="card-img-top img-fluid" alt="...">
             </div>
             <div class="card-body">
-                <span class="card-title">{{ nombre }}</span>
+                <div class="card-title">
+                    <span>{{ nombre }}</span>
+                </div>
                 <div class="items-card">
-                    <span class="item-card">{{ categoria }}</span>
-                    <span class="item-card">{{ empresaRepresentada }}</span>
-                    <span class="item-card">{{ puestoTrabajo }}</span>
-                    <span class="item-card">{{ sucursal }}</span>
-                    <span class="item-card">{{ telefono }}</span>
-                    <span class="item-card">{{ correo }}</span>
-                    <span class="item-card">{{ fechaRegistro }}</span>
-                    <span class="item-card">{{ sueldo }}</span>
+                    <div class="g-uno d-flex">
+                        <span class="item-card">{{ categoria }}</span>
+                        <span class="item-card">{{ empresaRepresentada }}</span>
+                    </div>
+                    <div class="g-dos d-flex">
+                        <span class="item-card">{{ puestoTrabajo }}</span>
+                        <span class="item-card">{{ sucursal }}</span>
+                    </div>
+                    <div class="g-tres d-flex">
+                        <span class="item-card">{{ telefono }}</span>
+                        <span class="item-card">{{ correo }}</span>
+                    </div>
+                    <div class="g-cuatro d-flex">
+                        <span class="item-card">{{ fechaRegistro }}</span>
+                        <span class="item-card">{{ sueldo }}</span>
+                    </div>
                     <p class="card-text">{{ descripcionProducto }}</p>
                 </div>
             </div>
@@ -25,15 +35,59 @@
 <style scoped>
 .card {
     margin: 4px;
+    border-radius: 15px;
 }
 
 .card:hover {
     filter: drop-shadow(2px 4px 6px #3f596bbf);
 }
 
-.encabezado-img {
-    width: 100%;
+.card .encabezado-img {
+    width: 80%;
     height: 30%;
+    margin: 0px auto;
+}
+
+.card .encabezado-img img {
+    border-top-right-radius: 25px;
+    border-top-left-radius: 25px;
+}
+
+.card .card-body {
+    padding: 0px 4px 5px 4px;
+}
+
+.card .card-body .card-title {
+    font-size: calc(0.75em + 0.1vw);
+    color: #3581B8;
+    margin: 0;
+    text-align: center;
+}
+
+.card .card-body .items-card {
+    flex-direction: column;
+}
+
+.card .card-body .items-card .g-uno {
+    justify-content: center;
+}
+
+.card .card-body .items-card .g-dos, .g-tres {
+    justify-content: space-around;
+}
+
+.card .card-body .items-card .g-tres {
+    justify-content: space-between;
+}
+.card .card-body .items-card .g-cuatro {
+    flex-direction: column;
+    justify-content: center;
+}
+
+.card .card-body .items-card .item-card,
+.card .card-body .items-card .card-text {
+    font-size: x-small;
+    text-align: center;
 }
 </style>
 
