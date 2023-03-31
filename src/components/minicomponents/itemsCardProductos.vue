5981f245c555157ac8eea88d05cf80bebe247b74
OnedGmez <oned.gomez@unah.hn>
2023-03-30 18:17:15 -0600

    Items de tarjeta para mostrarse en productos

---
+++ src/components/minicomponents/itemsCardProductos.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="items-card">
+        <div class="d-flex">
+            <span class="item-card"> {{ dataProducto['categoryname'] }} </span>
+        </div>
+        <p class="card-text"> {{ dataProducto['productdescription'] }} </p>
+    </div>
+</template>
+
+<script setup>
+const propsProducto = defineProps([
+    'dataProducto'
+])
+</script>
+
+<style scoped>
+.items-card div{
+    justify-content: center;
+}
+
+.items-card {
+    flex-direction: column;
+}
+
+.card .card-body .items-card .item-card,
+.card .card-body .items-card .card-text {
+    font-size: x-small;
+    text-align: center;
+    font-family: sans-serif;
+}
+</style>
