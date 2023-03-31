d823bf7db0faffa4f709bd2a8a8eac498d3da186
OnedGmez <oned.gomez@unah.hn>
2023-03-30 20:56:49 -0600

    contenido de productos para cuerpo del modal

---
+++ src/components/minicomponents/dataModalProductos.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="data">
+        <p class="data-text">{{ dataProducto['productdescription'] }}</p>
+        <div class="d-flex">
+            <span class="data-modal">{{ dataProducto['categoryname'] }}</span>
+            <span class="data-modal"> Lote: {{ dataProducto['lotnumber'] }} </span>
+        </div>
+        <div class="d-flex">
+            <span class="data-modal"> Precio: {{dataProducto['purchaseprice']}} </span>
+            <span class="data-modal"> Expira: {{ dataProducto['expirationdate'] }} </span>
+        </div>
+        <div class="d-flex">
+            <span class="data-modal"> {{ dataProducto['units'] }}/caja</span>
+            <span class="data-modal"> {{ dataProducto['stock'] }} </span>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+const propsProductoModal = defineProps([
+    'dataProducto'
+])
+</script>
+
+<style scoped>
+.data{
+    font-size: medium;
+    font-family: sans-serif;
+}
+
+.data .data-text{
+    text-align: center;
+}
+
+.data div{
+    justify-content: space-between;
+}
+</style>
