e28cc8ea572a306d0729da37ac8b4586dec26dfb
OnedGmez <oned.gomez@unah.hn>
2023-04-01 03:05:15 -0600

    Adición de variable con intención de ser global y funcional para manipular su valor

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import { defineStore } from 'pinia'
-import {ref} from 'vue'
+import { ref } from 'vue'
 import menuOpciones from '@/jsons/elementosMenu.json'
 
 export const generalStore = defineStore('store', () => {
   const menu = ref(menuOpciones.elementos);
+  const ordenarModo = ref(false)
+
+  const setModoOrden = () => {
+    ordenarModo.value = !ordenarModo.value
+  }
 
   return {
-    menu
+    menu,
+    setModoOrden,
+    ordenarModo
   }
 })
