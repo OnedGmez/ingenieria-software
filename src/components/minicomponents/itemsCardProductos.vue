ce2cf0bb62f630fb324fc29a3c95ad3d414b0552
OnedGmez <oned.gomez@unah.hn>
2023-04-01 21:53:09 -0600

    Media Query para tablet y Telefono

--- src/components/minicomponents/itemsCardProductos.vue
+++ src/components/minicomponents/itemsCardProductos.vue
@@ -14,18 +14,52 @@ const propsProducto = defineProps([
 </script>
 
 <style scoped>
-.items-card div{
+.items-card div {
     justify-content: center;
 }
 
 .items-card {
+    font-weight: 600;
     flex-direction: column;
+    text-align: center;
+    font-family: 'fredoka-family';
+    font-size: calc(.9em + 0.04vw);
 }
 
-.card .card-body .items-card .item-card,
-.card .card-body .items-card .card-text {
-    font-size: x-small;
-    text-align: center;
-    font-family: sans-serif;
+.card-text {
+    font-size: calc(.9em + 0.04vw) !important;
+    font-weight: 100!important;
+    line-height: normal;
+}
+
+/*
+* Media Query para tablet
+*/
+@media (max-width: 991.5px) {
+
+    .items-card {
+        font-size: calc(.5em + .7vw);
+    }
+
+    .card-text {
+        font-size: calc(.5em + .6vw) !important;
+    }
+
+}
+
+
+/*
+* Media Query para celular
+*/
+@media (max-width: 678px) {
+
+.items-card {
+    font-size: calc(.5em + 1.2vw);
+}
+
+.card-text {
+    font-size: calc(.5em + 1vw) !important;
+}
+
 }
 </style>
