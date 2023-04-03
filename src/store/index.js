0fbf0aa9d97b172f25f9e600655144abe033ec69
OnedGmez <oned.gomez@unah.hn>
2023-04-03 11:42:39 -0600

    Adición de funciones para filtrar las opciones del menú y para hacer el guardado en LocalStorage, cargar las categorias, sucursales

--- src/store/index.js
+++ src/store/index.js
@@ -2,25 +2,84 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import CryptoJS from 'crypto-js'
 import menuOpciones from '@/jsons/elementosMenu.json'
+import { useUsuarioStore } from './usuario.js'
+import { supabase } from '@/lib/supabaseClient'
 
 export const generalStore = defineStore('store', () => {
-
+  const storeUsuario = useUsuarioStore()
   const menu = ref(menuOpciones.elementos);
   const ordenarModo = ref(false)
   const nuevoMenu = ref([])
 
-  const filtrar = (rol) =>{
+  const filtrar = (rol) => {
     nuevoMenu.value = []
     for (let i = 0; i < (menu.value).length; i++) {
       const element = (menu.value)[i];
-      const index = (JSON.stringify(element,['permitidoPara'])).indexOf(rol)
-      if(index !== -1){
+      const index = (JSON.stringify(element, ['permitidoPara'])).indexOf(rol)
+      if (index !== -1) {
         nuevoMenu.value.push(element)
       }
     }
   }
 
+  const guardarLocalStorage = (data, key) => {
+    if (key === 'usuario') {
+      data['rol'] = encriptarData(data['rol'], 'rol')
+      const usuario = ref([
+        {
+          rol: data['rol'],
+          nombreusuario: data['nombreUsuario'],
+          urlphoto: data['urlphoto']
+        }
+      ])
+      storeUsuario.crearToken(JSON.stringify(usuario.value))
+    }
+    localStorage.setItem(key, JSON.stringify(data))
+  }
 
+  const cargaCategorias = async () => {
+    try {
+      let { data: categories, error } = await supabase
+        .from('categories')
+        .select('*')
+
+      if (error instanceof Error) {
+        alert(error)
+      }
+
+      if (categories != '') {
+        guardarLocalStorage(categories, 'categorias')
+      } else {
+        alert('Categorias vacías')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+  cargaCategorias()
+
+  const cargarSucursales = async () => {
+    try {
+
+      let { data: sucursals, error } = await supabase
+        .from('sucursals')
+        .select('sucursalcode,sucursalname')
+
+
+      if (error instanceof Error) {
+        alert(error)
+      }
+
+      if (sucursals != '') {
+        guardarLocalStorage(sucursals, 'sucursales')
+      } else {
+        alert('Sucursales vacías')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+  cargarSucursales()
 
 
   /**
@@ -49,6 +108,7 @@ export const generalStore = defineStore('store', () => {
     ordenarModo,
     encriptarData,
     desencriptarData,
-    filtrar
+    filtrar,
+    guardarLocalStorage
   }
 })
