d78c0536f0794c0074f3b60b23514ba7c7f9a22a
OnedGmez <oned.gomez@unah.hn>
2023-04-03 03:07:40 -0600

    Instalación de VueCryptojs

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueCryptojs from 'vue-cryptojs'
 library.add(fas);
 
 //Importamos librerias/frameworks externos instalados
@@ -20,5 +21,6 @@ const app = createApp(App).use(router).use(bootstrap)
 app.config.globalProperties.axios=axios
 app.use(pinia)
 app.component('font-awesome-icon',FontAwesomeIcon)
+app.use(VueCryptojs)
 app.mount('#app')
 
