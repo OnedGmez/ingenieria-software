05c750797af58e79288c36a108091184b68bfc35
OnedGmez <oned.gomez@unah.hn>
2023-04-03 03:03:25 -0600

    Función para filtrar las opciones del menú según el rol admitido

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,54 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import CryptoJS from 'crypto-js'
 import menuOpciones from '@/jsons/elementosMenu.json'
 
 export const generalStore = defineStore('store', () => {
+
   const menu = ref(menuOpciones.elementos);
   const ordenarModo = ref(false)
+  const nuevoMenu = ref([])
 
+  const filtrar = (rol) =>{
+    nuevoMenu.value = []
+    for (let i = 0; i < (menu.value).length; i++) {
+      const element = (menu.value)[i];
+      const index = (JSON.stringify(element,['permitidoPara'])).indexOf(rol)
+      if(index !== -1){
+        nuevoMenu.value.push(element)
+      }
+    }
+  }
+
+
+
+
+  /**
+   * Función que nos ayuda a cambiar el valor del modo para ordenar productos
+   */
   const setModoOrden = () => {
     ordenarModo.value = !ordenarModo.value
   }
 
+
+  /**
+   * Funciones de utilidad pública
+   */
+  const encriptarData = (data, key) => {
+    return CryptoJS.AES.encrypt(data, key).toString();
+  }
+
+  const desencriptarData = (data, key) => {
+    return CryptoJS.AES.decrypt(data, key).toString(CryptoJS.enc.Utf8);
+  }
+
+
   return {
-    menu,
+    nuevoMenu,
     setModoOrden,
-    ordenarModo
+    ordenarModo,
+    encriptarData,
+    desencriptarData,
+    filtrar
   }
 })
