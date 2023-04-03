fb01c6979c51588d7edfab686847e4baf1fa868f
OnedGmez <oned.gomez@unah.hn>
2023-04-03 11:43:32 -0600

    Función para guardar en localStorage movida a index.js

--- src/store/usuario.js
+++ src/store/usuario.js
@@ -10,19 +10,6 @@ import { SHA256 } from 'crypto-js'
 export const useUsuarioStore = defineStore("usuariosStore", () => {
     const pub = generalStore()
 
-    const guardarLocalStorage = (data) => {
-        data['rol'] = pub.encriptarData(data['rol'], 'rol')
-        const usuario = ref([
-            {
-                rol: data['rol'],
-                nombreusuario: data['nombreUsuario'],
-                urlphoto: data['urlphoto']
-            }
-        ])
-        crearToken(JSON.stringify(usuario.value))
-        localStorage.setItem('usuario', JSON.stringify(data))
-    }
-
     const crearToken = (data) => {
         const header = {
             "alg": "HS256",
@@ -35,12 +22,15 @@ export const useUsuarioStore = defineStore("usuariosStore", () => {
             (data),
             firma);
 
-        sessionStorage.setItem('token',data)
+        sessionStorage.setItem('token', data)
     }
+
+
+
     /*
     description: Ponemos a disposición todos los elementos que deseamos que sean accedidos desde fuera.
     */
     return {
-        guardarLocalStorage
+        crearToken
     }
 })
