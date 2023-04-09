a2f0d6c718412ddb1744da3db93262040b286f08
OnedGmez <oned.gomez@unah.hn>
2023-04-09 12:10:19 -0600

    Adición y configuración de funciones para validar la sesión

--- src/store/usuario.js
+++ src/store/usuario.js
@@ -10,6 +10,15 @@ import { SHA256 } from 'crypto-js'
 export const useUsuarioStore = defineStore("usuariosStore", () => {
     const pub = generalStore()
 
+    /**
+     * dev: Oned Gómez
+     * Función que nos ayudará a crear el token de sesión para cada usuario en los inicios de sesión
+     * @param {*} data recibe la información del usuario que inició sesión
+     * @constant {*} header Fragmento del token (siguiendo la idea de JSON WEB TOKEN)
+     * @constant {*} firma Segundo fragmento del token, básicamente representa a quien pertenece el token
+     * @constant {*} token Es el token ya cifrado con la normativa de JWT
+     * El token se guarda en la SessionStorage
+     */ 
     const crearToken = (data) => {
         const header = {
             "alg": "HS256",
@@ -22,7 +31,7 @@ export const useUsuarioStore = defineStore("usuariosStore", () => {
             (data),
             firma);
 
-        sessionStorage.setItem('token', data)
+        sessionStorage.setItem('token', token)
     }
 
 
