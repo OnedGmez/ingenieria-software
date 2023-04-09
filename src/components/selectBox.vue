a6e68a48c110b108fb77b5fa87e20b40332aec26
OnedGmez <oned.gomez@unah.hn>
2023-04-09 11:54:23 -0600

    Retoque en las funciones que emite los códigos de la información que tendrá

--- src/components/selectBox.vue
+++ src/components/selectBox.vue
@@ -1,21 +1,20 @@
 <template>
     <div class="select-group d-flex">
         <label for="disabledSelect"> {{ label }}</label>
-        <select @change="filtrar" id="disabledSelect" class="form-select">
+        <select v-model="valor" @change="emitirCode" id="disabledSelect" class="form-select">
             <option v-for="elemento in data" :value="elemento['code']"> {{ elemento['name'] }} </option>
         </select>
     </div>
 </template>
 
 <style scoped>
-
-.select-group{
+.select-group {
     flex-direction: column;
     font-family: 'fredoka-family';
     font-size: calc(.9em + 0.04vw) !important;
 }
 
-.select-group .form-select{
+.select-group .form-select {
     border-radius: 25px 25px 25px 25px;
     border: none;
     border-bottom: #3581B8 solid 2px;
@@ -24,23 +23,30 @@
 }
 
 
-.select-group .form-select:focus{
+.select-group .form-select:focus {
     box-shadow: none;
+}
+
+label {
+    font-weight: 500;
 }
 </style>
 
 <script setup>
 import { ref, defineEmits } from "vue";
+const valor = ref('')
+const emisiones = defineEmits(['codigo'])
+const categoria = ref('')
+const sucursal = ref('')
 
-const elementos = ref('')
-let catTMP;
-
-const propsTarjeta = defineProps([
+const propsSelectBox = defineProps([
     'data',
-    'label'
+    'label',
+    'seleccionado'
 ])
 
-const filtrar = (event) =>{
-    console.log(event.target.value)
+const emitirCode = () => {
+    emisiones('codigo', valor.value)
 }
+
 </script>
