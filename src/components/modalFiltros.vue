4679963ad7864c044de615d69ba6c295bab5b4aa
OnedGmez <oned.gomez@unah.hn>
2023-04-09 14:07:06 -0600

    Filtros funcionales (falta agregar etiquetas para ver los filtros aplicados)

--- src/components/modalFiltros.vue
+++ src/components/modalFiltros.vue
@@ -5,16 +5,19 @@
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="filtros-secundarios">
-                        <selectBox class="filtro" label="Categorias:" :data="categorias" />
-                        <selectBox class="filtro" label="Sucursales:" :data="sucursales" />
+                        <selectBox class="filtro" label="Sucursales:" :data="sucursales" :seleccionado="sucursal"
+                            @codigo="(codigoC) => modificarCodigoSucursal(codigoC)" />
                         <div class="filtro-existencia d-flex filtro">
-                            <input type="checkbox" name="checkExistencia" id="existencia">
-                            <label for="existencia">Sin existencias</label>
+                            <input v-model="available" type="checkbox"
+                                name="checkExistencia" id="existencia">
+                            <label for="existencia">En existencias</label>
                         </div>
+                        <selectBox class="filtro" label="Categorias:" :data="categorias" :seleccionado="categoria"
+                            @codigo="(codigoC) => modificarCodigoCategoria(codigoC)" />
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-cerrar">Aplicar</button>
+                    <button type="button" class="btn btn-cerrar" @click="aplicarFiltros">Aplicar</button>
                     <button type="button" class="btn btn-cerrar" @click="cerrarModal">Cancelar</button>
                     <!--Emitimos un evento mediante una función al contenedor padre para avisar que lo queremos cerrar-->
                 </div>
@@ -82,6 +85,7 @@
 label {
     font-size: calc(.9em + 0.04vw) !important;
     font-family: 'fredoka-family';
+    font-weight: 500;
 }
 </style>
 
@@ -89,24 +93,83 @@ label {
 import selectBox from '@/components/selectBox.vue'
 import { ref } from 'vue';
 //Definimos los emits necesarios con sus respectivas funciones
-const emisiones = defineEmits(['ocultarModal'])
-
+const emisiones = defineEmits(['ocultarModal', 'aplicarFiltros'])
 const categorias = ref('')
 const sucursales = ref('')
+
+//Constantes reactivas que se utilizaran para los filtros
+const available = ref('')
+const categoria = ref('')
+const sucursal = ref('')
+const existencia = ref('')
 
 const cerrarModal = () => {
     emisiones('ocultarModal')
 }
 
+/**
+ * dev: Oned Gomez
+ * Funcion que sirve para emitir el evento de aplicación de filtros a la información, adicional se guarda el valor de los filtros al localStorage para ayudar a la persistencia de esos filtros
+ */
+const aplicarFiltros = () => {
+    emisiones('aplicarFiltros', available.value, categoria.value, sucursal.value)
+    localStorage.setItem('filtro-existencia', available.value)
+    localStorage.setItem('filtro-categoria', categoria.value)
+    localStorage.setItem('filtro-sucursal', sucursal.value)
+    emisiones('ocultarModal')
+}
+
+const modificarCodigoCategoria = (codigoC) => {
+    categoria.value = codigoC
+}
+
+const modificarCodigoSucursal = (codigoC) => {
+    sucursal.value = codigoC
+}
+
+
+/**
+ * dev: Oned Gómez
+ * Función para inicializar el valor de los filtros cada vez que aparezca el modal (hacer que los aplicados filtros prevalezcan entre cada aparición del modal)
+ */
+/**
+ * dev: Oned Gómez
+ * Función para inicializar el valor de los filtros cada vez que aparezca el modal (hacer que los aplicados filtros prevalezcan entre cada aparición del modal)
+ */
+ const iniciarlizarFiltros = () => {
+    if (localStorage.getItem('filtro-existencia') !== null) {
+        available.value = localStorage.getItem('filtro-existencia')
+    } else {
+        available.value = true
+    }
+
+    if (localStorage.getItem('filtro-categoria') !== null) {
+        categoria.value = localStorage.getItem('filtro-categoria')
+    } else {
+        categoria.value = ''
+    }
+
+    if (localStorage.getItem('filtro-sucursal') !== null) {
+        sucursal.value = localStorage.getItem('filtro-sucursal')
+    } else {
+        sucursal.value = ''
+    }
+}
+iniciarlizarFiltros()
+/**
+ * dev: Oned Gomez
+ * Función para cargar la información de las categorias y sucursales a los filtros
+ */
 const cargarSelect = () => {
     categorias.value = localStorage.getItem('categorias');
-    categorias.value = categorias.value.replaceAll('"categorycode":','"code":')
-    categorias.value = categorias.value.replaceAll('"categoryname":','"name":')
+    categorias.value = categorias.value.replaceAll('"categorycode":', '"code":')
+    categorias.value = categorias.value.replaceAll('"categoryname":', '"name":')
     categorias.value = JSON.parse(categorias.value)
 
+
     sucursales.value = localStorage.getItem('sucursales');
-    sucursales.value = sucursales.value.replaceAll('"sucursalcode":','"code":')
-    sucursales.value = sucursales.value.replaceAll('"sucursalname":','"name":')
+    sucursales.value = sucursales.value.replaceAll('"sucursalcode":', '"code":')
+    sucursales.value = sucursales.value.replaceAll('"sucursalname":', '"name":')
     sucursales.value = JSON.parse(sucursales.value)
 }
 cargarSelect()
