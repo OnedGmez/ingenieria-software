def7757e8a37a9024ef9ff63dfa22dac85fdce89
OnedGmez <oned.gomez@unah.hn>
2023-04-17 21:39:50 -0600

    Nuevo parametro para la ruta y ayudar a la persistencia de la selección en el menu

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,22 @@ const routes = [
     alias: '/'
   },
   {
-    path: '/inventario/:rol/:sucursalcode',
+    path: '/inventario/:id/:rol/:sucursalcode',
     name: 'inventario',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/inventarioView.vue')
+    }
+  },{
+    path: '/proveedores/:id/:rol/:sucursalcode',
+    name: 'proveedores',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/proveedoresView.vue')
     }
   }
 ]
