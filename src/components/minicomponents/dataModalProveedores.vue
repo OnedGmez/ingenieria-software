19a73e27ffb61fc84e6dce0c336b2086be6010d9
OnedGmez <oned.gomez@unah.hn>
2023-04-17 23:56:25 -0600

    Complemento del modal para mostrar la información del proveedor

---
+++ src/components/minicomponents/dataModalProveedores.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="data">
+        <div class="d-flex">
+            <span class="data-modal"> Representante: <span class="data-modal-valor">{{ dataProveedor['representativename'] }}</span> </span>
+        </div>
+        <div class="d-flex">
+            <span class="data-modal"> Código: <span class="data-modal-valor">{{ dataProveedor['vendorcode'] }}</span></span>
+            <span class="data-modal"> País: <span class="data-modal-valor">{{ dataProveedor['country'] }}</span></span>
+        </div>
+        <div class="d-flex">
+            <span class="data-modal">Inicio de relación: <span class="data-modal-valor">{{ dataProveedor['relationshipinitiation'] }}</span> </span>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+const propsProveedorModal = defineProps([
+    'dataProveedor'
+])
+</script>
+
+<style scoped>
+.data{
+    font-family: 'fredoka-family';
+    font-size: calc(.9em + 0.04vw) !important;
+}
+
+.data .data-modal{
+    font-weight: 400;
+}
+
+.data .data-modal .data-modal-valor{
+    font-weight: 100;
+}
+
+.data div{
+    justify-content: space-between;
+}
+
+.data div:first-child, .data div:last-child{
+    justify-content: center;
+}
+</style>
