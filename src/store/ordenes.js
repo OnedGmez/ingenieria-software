f859b86d606b4a4476d14f5bd6534d5f2aa71321
OnedGmez <oned.gomez@unah.hn>
2023-04-27 00:47:58 -0600

    Ordenes con watcheffects

--- src/store/ordenes.js
+++ src/store/ordenes.js
@@ -12,6 +12,24 @@ export const useOrdenStore = defineStore("OrdenStore", () => {
     const store = generalStore()
     const respuesta = ref('')
 
+
+    watchEffect(() => {
+        const orders = supabase.channel('custom-all-channel')
+            .on(
+                'postgres_changes',
+                { event: '*', schema: 'public', table: 'orders' },
+                (payload) => {
+                    console.log(dataNoFiltradaOrdenes.value)
+                    dataNoFiltradaOrdenes.value = (dataNoFiltradaOrdenes.value).filter(orden => {
+                        return orden.ordercode != payload.old['ordercode']
+                    })
+                    console.log(dataNoFiltradaOrdenes.value)
+                }
+            )
+            .subscribe()
+        return () => supabase.removeChannel(orders);
+    })
+
     return {
         dataNoFiltradaOrdenes
     }
