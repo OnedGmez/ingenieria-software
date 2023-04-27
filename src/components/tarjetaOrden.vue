de41c180782fb092ac550cc78442d569624e7891
OnedGmez <oned.gomez@unah.hn>
2023-04-26 23:37:27 -0600

    Tarjeta actualizada

--- src/components/tarjetaOrden.vue
+++ src/components/tarjetaOrden.vue
@@ -1,35 +1,31 @@
 <template>
     <div class="contenedor col-12">
-        <div class="contenido-tarjeta">
+        <div @click="abrirDetalles" class="contenido-tarjeta">
             <div class="informacion">
                 <div class="cabecera-informacion">
-                    <span class="key-texto"> Código: <span class="resultado-texto"> 1151asabxasd</span> </span>
-                    <span class="key-texto"> Fecha: <span class="resultado-texto"> 2023-30-12</span> </span>
-                    <span class="key-texto"> Tipo de orden: <span class="resultado-texto"> Pedido </span> </span>
+                    <span class="key-texto"> Código: <span class="resultado-texto"> {{ data['ordercode'] }}</span> </span>
+                    <span class="key-texto"> Fecha: <span class="resultado-texto"> {{ data['shipmentdate'] }}</span> </span>
+                    <span class="key-texto"> Tipo de orden: <span class="resultado-texto"> {{ data['descriptiontype'] }} </span> </span>
                 </div>
                 <div class="d-flex detalle-informacion">
                     <div class="detalle-pedido-sucursales-central">
                         <div class="detalle-sucursal-pedido">
-                            <span class="key-texto"> Realizada por: <span class="resultado-texto"> Byron Eliel
-                                    Gómez</span></span>
-                            <span class="key-texto"> Sucursal: <span class="resultado-texto"> Sucursal #2</span></span>
+                            <span class="key-texto"> Realizada por: <span class="resultado-texto"> {{ data['sender'] }} </span></span>
+                            <span class="key-texto"> Sucursal: <span class="resultado-texto"> {{ data['sucursalname'] }} </span></span>
                         </div>
                         <div class="detalle-central-pedido">
-                            <span class="key-texto"> Revisada por: <span class="resultado-texto"> Oned Alejandro
-                                    Gómez</span></span>
-                            <span class="key-texto"> Cancelada por: <span class="resultado-texto"> Oned Alejandro
-                                    Gómez</span></span>
-                            <span class="key-texto"> Fecha de revisión: <span class="resultado-texto">
-                                    2023-30-12</span></span>
+                            <span v-if="data['receipdate'] != null" class="key-texto"> Fecha de revisión: <span class="resultado-texto">
+                                    {{ data['receiptdate'] }}</span></span>
                         </div>
                     </div>
                     <div class="estatus">
-                        <span> <font-awesome-icon icon="circle" /> </span>
+                        <span :class="[data['status'] == 'G'?'generada': data['status'] == 'R'?'rechazada':data['status'] == 'C'?'cancelada': data['status'] =='A'?'aceptada': data['status']=='D'?'descargada':'' ]"> <font-awesome-icon icon="circle" /> </span>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <modalDetalleOrden v-if="mostrandoModal == true" :data="data" @cerrarmodal="() => abrirDetalles()"/>
 </template>
 
 <style scoped>
@@ -85,3 +81,19 @@
     color: #3581B8;
 }
 </style>
+
+<script setup>
+import modalDetalleOrden from './modalDetalleOrden.vue';
+import { ref } from 'vue';
+
+const mostrandoModal = ref(false)
+
+const propsOrden = defineProps([
+    'data'
+])
+
+const abrirDetalles = () =>{
+    mostrandoModal.value = !mostrandoModal.value
+}
+console.log(propsOrden.data)
+</script>
